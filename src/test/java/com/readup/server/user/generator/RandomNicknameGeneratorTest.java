15868b916f47b24fc7c68313806b01986c2db000
Hut234 <doitdoitdoitdoitdo@gmail.com>
2025-03-01 21:34:55 +0900

    chore: Change coding convention to Naver

--- src/test/java/com/readup/server/user/generator/RandomNicknameGeneratorTest.java
+++ src/test/java/com/readup/server/user/generator/RandomNicknameGeneratorTest.java
@@ -1,30 +1,29 @@
 package com.readup.server.user.generator;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RandomNicknameGeneratorTest {
 
-  @Test
-  @DisplayName("generate: 랜덤 닉네임 생성 메서드")
-  void generate_success() {
-  	// when
-    String generatedRandomNickname = RandomNicknameGenerator.generate();
+	@Test
+	@DisplayName("generate: 랜덤 닉네임 생성 메서드")
+	void generate_success() {
+		// when
+		String generatedRandomNickname = RandomNicknameGenerator.generate();
 
-    boolean isAdjectiveContained = RandomNicknameGenerator.ADJECTIVES.stream()
-        .anyMatch(generatedRandomNickname::startsWith);
+		boolean isAdjectiveContained = RandomNicknameGenerator.ADJECTIVES.stream()
+			.anyMatch(generatedRandomNickname::startsWith);
 
-    boolean isNounContained = RandomNicknameGenerator.NOUNS.stream()
-        .anyMatch(generatedRandomNickname::endsWith);
+		boolean isNounContained = RandomNicknameGenerator.NOUNS.stream()
+			.anyMatch(generatedRandomNickname::endsWith);
 
-    // then
-    assertNotNull(generatedRandomNickname);
-    assertTrue(isAdjectiveContained);
-    assertTrue(isNounContained);
-    assertThat(generatedRandomNickname.length()).isBetween(2, 12);
-  }
+		// then
+		assertNotNull(generatedRandomNickname);
+		assertTrue(isAdjectiveContained);
+		assertTrue(isNounContained);
+		assertThat(generatedRandomNickname.length()).isBetween(2, 12);
+	}
 }
