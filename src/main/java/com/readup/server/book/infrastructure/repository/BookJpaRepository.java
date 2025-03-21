4cb80bb32d04e289c3a9fb06c01450da3c2d7998
POKUDING <yoseb95@gmail.com>
2025-03-21 17:21:15 +0900

    feat: BookJpaRepository #2

---
+++ src/main/java/com/readup/server/book/infrastructure/repository/BookJpaRepository.java
@@ -0,0 +1,8 @@
+package com.readup.server.book.infrastructure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.readup.server.book.domain.Book;
+
+public interface BookJpaRepository extends JpaRepository<Book, Long> {
+}
