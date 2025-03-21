0d4025456ca3144839ca10904dde5e3f8e569d71
POKUDING <yoseb95@gmail.com>
2025-03-21 17:21:03 +0900

    feat: getBookInfo method #2

--- src/main/java/com/readup/server/book/application/BookInfoService.java
+++ src/main/java/com/readup/server/book/application/BookInfoService.java
@@ -2,8 +2,10 @@ package com.readup.server.book.application;
 
 import org.springframework.stereotype.Service;
 
+import com.readup.server.book.domain.Book;
 import com.readup.server.book.infrastructure.client.bookinfo.BookInfoClientFactory;
 import com.readup.server.book.infrastructure.client.bookinfo.vo.BookInfoVO;
+import com.readup.server.book.infrastructure.repository.BookJpaRepository;
 import com.readup.server.book.presentation.dto.GetExternalBookResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -13,8 +15,14 @@ import lombok.RequiredArgsConstructor;
 public class BookInfoService {
 	private static final String SERVICE_NAME = "NationalLibraryOfKorea";
 	private final BookInfoClientFactory bookInfoClientFactory;
+	private final BookJpaRepository bookJpaRepository;
 
 	public GetExternalBookResponse getBookInfo(String isbn) {
+		
 		BookInfoVO bookInfoVO = bookInfoClientFactory.getBookInfoClient(SERVICE_NAME).getBookInfo(isbn);
+		Book newBook = bookInfoVO.toEntity();
+
+		Book savedBook = bookJpaRepository.save(newBook);
+		return GetExternalBookResponse.from(savedBook);
 	}
 }
