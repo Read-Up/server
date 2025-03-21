28c56e24b893629478b2205285b5928f2a1d9f91
POKUDING <yoseb95@gmail.com>
2025-03-21 17:16:48 +0900

    fix: Book Entity #2

--- src/main/java/com/readup/server/book/domain/Book.java
+++ src/main/java/com/readup/server/book/domain/Book.java
@@ -5,19 +5,30 @@ import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "book")
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor
 public class Book {
+
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	Long id;
 
-	@Column(name = "title", nullable = false)
+	@Column(name = "title", nullable = false, unique = true)
 	String title;
 
 	@Column(name = "author", nullable = false)
@@ -26,13 +37,13 @@ public class Book {
 	@Column(name = "publisher", nullable = false)
 	String publisher;
 
-	@Column(name = "isbn", nullable = false)
+	@Column(name = "isbn", nullable = false, unique = true)
 	String isbn;
 
-	@Column(name = "title_url", nullable = false)
+	@Column(name = "title_url")
 	String titleUrl;
 
-	@Column(name = "summary", nullable = false)
+	@Column(name = "summary")
 	String summary;
 
 	@OneToMany(mappedBy = "book", fetch = FetchType.EAGER)
