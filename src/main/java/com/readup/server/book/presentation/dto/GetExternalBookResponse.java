49e65db8ab766dd6c9a851b32d722710aead4ba9
POKUDING <yoseb95@gmail.com>
2025-03-21 17:19:38 +0900

    feat: add Entity to Dto Mapping method #2

--- src/main/java/com/readup/server/book/presentation/dto/GetExternalBookResponse.java
+++ src/main/java/com/readup/server/book/presentation/dto/GetExternalBookResponse.java
@@ -2,6 +2,12 @@ package com.readup.server.book.presentation.dto;
 
 import java.util.List;
 
+import com.readup.server.book.domain.Book;
+import com.readup.server.book.domain.Chapter;
+
+import lombok.Builder;
+
+@Builder
 public record GetExternalBookResponse(
 	Long bookId,
 	String bookTitle,
@@ -10,4 +16,14 @@ public record GetExternalBookResponse(
 	String isbn,
 	List<String> tableOfContents
 ) {
+	public static GetExternalBookResponse from(Book savedBook) {
+		return GetExternalBookResponse.builder()
+			.bookId(savedBook.getId())
+			.bookTitle(savedBook.getTitle())
+			.publisher(savedBook.getPublisher())
+			.author(savedBook.getAuthor())
+			.isbn(savedBook.getIsbn())
+			.tableOfContents(savedBook.getChapterList().stream().map(Chapter::getName).toList())
+			.build();
+	}
 }
