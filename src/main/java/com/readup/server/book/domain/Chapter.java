502b8659ee894e5b6b09da6771a9153bd251cb6d
POKUDING <yoseb95@gmail.com>
2025-03-21 17:20:30 +0900

    feat: add GeneratedValue in id #2

--- src/main/java/com/readup/server/book/domain/Chapter.java
+++ src/main/java/com/readup/server/book/domain/Chapter.java
@@ -2,15 +2,27 @@ package com.readup.server.book.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "chapter")
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class Chapter {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	Long id;
 
 	@Column(name = "name", nullable = false)
