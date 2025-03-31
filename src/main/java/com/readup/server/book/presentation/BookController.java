f3df43cab2fa8e06c08ea53caaf90ebdff182246
POKUDING <yoseb95@gmail.com>
2025-03-31 12:57:45 +0900

    feat: ChapterList 수정 endpoint #15

--- src/main/java/com/readup/server/book/presentation/BookController.java
+++ src/main/java/com/readup/server/book/presentation/BookController.java
@@ -1,17 +1,25 @@
 package com.readup.server.book.presentation;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.readup.server.book.application.BookCommandService;
 import com.readup.server.book.presentation.dto.UpdateChapterListRequest;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/books")
 public class BookController {
 
-	@GetMapping
-	public void updateChapter(@RequestBody UpdateChapterListRequest request) {
+	private final BookCommandService bookCommandService;
+
+	@PutMapping("/{bookId}/chapters")
+	public void updateChapterList(@PathVariable Long bookId, @RequestBody UpdateChapterListRequest request) {
+		bookCommandService.updateChapterList(request);
 	}
 }
