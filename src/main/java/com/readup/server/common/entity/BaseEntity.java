59cc99e9eef0a85e95a26041a04eae30afd5072d
POKUDING <yoseb95@gmail.com>
2025-04-09 17:47:56 +0900

    feat: add BaseEntity columns in schema.sql and BaseEntity.java #15

--- src/main/java/com/readup/server/common/entity/BaseEntity.java
+++ src/main/java/com/readup/server/common/entity/BaseEntity.java
@@ -2,7 +2,9 @@ package com.readup.server.common.entity;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -18,7 +20,18 @@ public abstract class BaseEntity {
 	@Column(name = "created_at", updatable = false)
 	private LocalDateTime createdAt;
 
+	@CreatedBy
+	@Column(name = "created_by", updatable = false)
+	private Long createdBy;
+
+	@LastModifiedBy
+	@Column(name = "updated_by")
+	private Long updatedBy;
+
 	@LastModifiedDate
 	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
 }
