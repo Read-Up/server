2b800c6f7d370f194ef0da1aaa854077690d4ce5
POKUDING <yoseb95@gmail.com>
2025-04-14 14:15:00 +0900

    refactor: change DEFAULT_SUCCESS_MESSAGE to final #15

--- src/main/java/com/readup/server/common/dto/ApiResponse.java
+++ src/main/java/com/readup/server/common/dto/ApiResponse.java
@@ -2,7 +2,7 @@ package com.readup.server.common.dto;
 
 public record ApiResponse<T>(boolean success, T data, String message) {
 
-	public static String DEFAULT_SUCCESS_MESSAGE = "Request succeeded.";
+	public static final String DEFAULT_SUCCESS_MESSAGE = "Request succeeded.";
 
 	public static <T> ApiResponse<T> successResponse(T data, String message) {
 		return new ApiResponse<>(true, data, message);
