f84a41c2c526adbf6088eec86ec867cbf399bc42
POKUDING <yoseb95@gmail.com>
2025-04-16 19:43:37 +0900

    feat: BookController searchBook method #29

--- src/main/java/com/readup/server/book/presentation/BookController.java
+++ src/main/java/com/readup/server/book/presentation/BookController.java
@@ -1,5 +1,9 @@
 package com.readup.server.book.presentation;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.readup.server.book.application.BookCommandService;
+import com.readup.server.book.application.BookQueryService;
+import com.readup.server.book.presentation.dto.GetBookResponse;
+import com.readup.server.book.presentation.dto.SearchBookRequest;
 import com.readup.server.book.presentation.dto.UpdateChapterListRequest;
 import com.readup.server.common.dto.ApiResponse;
 
@@ -18,11 +25,24 @@ import lombok.RequiredArgsConstructor;
 public class BookController {
 
 	private final BookCommandService bookCommandService;
+	private final BookQueryService bookQueryService;
+
+	@GetMapping
+	public ApiResponse<PagedModel<GetBookResponse>> searchBook(SearchBookRequest searchBookRequest,
+		@PageableDefault Pageable pageable) {
+
+		PagedModel<GetBookResponse> getBookResponsePagedModel =
+			bookQueryService.searchBook(searchBookRequest, pageable);
+
+		return ApiResponse.successResponse(getBookResponsePagedModel);
+	}
 
 	@PutMapping("/{bookId}/chapters")
 	public ApiResponse<Void> updateChapterList(@PathVariable Long bookId,
 		@RequestBody UpdateChapterListRequest request) {
+		
 		bookCommandService.updateChapterList(bookId, request);
+
 		return ApiResponse.successResponse();
 	}
 }
