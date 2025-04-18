6b35bd4b12cbbab0296ead1a35347cac133891cf
JunHyung Park <yoseb95@gmail.com>
2025-04-18 12:24:05 +0900

    [Feature] 책 검색 API  #29 (#33)
    * feat: GetBookResponse #29
    
    * feat: QuerydslConfig #29
    
    * feat: SearchBookRequest #29
    
    * feat: BookController searchBook method #29
    
    * feat: BookQueryService #29
    
    * feat: BookRepositoryImpl #29
    
    * test: Search Book Test #29
    
    * refactor: /api prefix 전역 설정 #29

---
+++ src/main/java/com/readup/server/book/infrastructure/repository/BookRepositoryImpl.java
@@ -0,0 +1,80 @@
+package com.readup.server.book.infrastructure.repository;
+
+import static com.readup.server.book.domain.QBook.*;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.readup.server.book.domain.Book;
+import com.readup.server.book.domain.repository.BookRepository;
+import com.readup.server.book.presentation.dto.GetBookResponse;
+import com.readup.server.common.exception.ErrorCode;
+import com.readup.server.common.exception.RepositoryException;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class BookRepositoryImpl implements BookRepository {
+
+	private final JPAQueryFactory jpaQueryFactory;
+	private final BookJpaRepository bookJpaRepository;
+
+	@Override
+	public Book save(Book newBook) {
+		return bookJpaRepository.save(newBook);
+	}
+
+	@Override
+	public boolean existsByIsbnOrTitle(String isbn, String title) {
+		return bookJpaRepository.existsByIsbnOrTitle(isbn, title);
+	}
+
+	@Override
+	public Book getBookById(Long id) {
+		return bookJpaRepository.findBookById(id)
+			.orElseThrow(() -> new RepositoryException(ErrorCode.BOOK_NOT_FOUND));
+	}
+
+	@Override
+	public Page<GetBookResponse> searchBook(String title, String isbn, Pageable pageable) {
+		List<Book> bookList = getBookListByTitleOrIsbn(title, isbn, pageable);
+		List<GetBookResponse> getBookResponseList = bookList.stream()
+			.map(GetBookResponse::from)
+			.toList();
+
+		JPAQuery<Long> countQuery = getCountQueryByTitleOrIsbn(title, isbn);
+
+		return PageableExecutionUtils.getPage(getBookResponseList, pageable, countQuery::fetchOne);
+	}
+
+	private JPAQuery<Long> getCountQueryByTitleOrIsbn(String title, String isbn) {
+		return jpaQueryFactory.select(book.count())
+			.from(book)
+			.where(searchBookByTitleOrIsbnPredicate(title, isbn));
+	}
+
+	private List<Book> getBookListByTitleOrIsbn(String title, String isbn, Pageable pageable) {
+		return jpaQueryFactory.select(book)
+			.from(book)
+			.where(searchBookByTitleOrIsbnPredicate(title, isbn))
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+	}
+
+	private Predicate searchBookByTitleOrIsbnPredicate(String title, String isbn) {
+		return ExpressionUtils.allOf(
+			title != null ? book.title.contains(title) : null,
+			isbn != null ? book.isbn.contains(isbn) : null
+		);
+	}
+}
---
+++ src/test/java/com/readup/server/book/application/BookQueryServiceTest.java
@@ -0,0 +1,68 @@
+package com.readup.server.book.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+
+import com.readup.server.book.domain.repository.BookRepository;
+import com.readup.server.book.presentation.dto.GetBookResponse;
+import com.readup.server.book.presentation.dto.SearchBookRequest;
+
+@ExtendWith(MockitoExtension.class)
+class BookQueryServiceTest {
+
+	@InjectMocks
+	private BookQueryService bookQueryService;
+
+	@Mock
+	private BookRepository bookRepository;
+
+	@Nested
+	@DisplayName("책 검색 테스트")
+	class SearchBookTest {
+
+		@Test
+		@DisplayName("책 검색 성공")
+		void searchBookSuccessTest() {
+			// given
+			String title = "testTitle";
+			String isbn = "1234567890";
+			SearchBookRequest searchBookRequest = new SearchBookRequest(title, isbn);
+			GetBookResponse getBookResponse = GetBookResponse.builder()
+				.bookId(1L)
+				.title(title)
+				.author("testAuthor")
+				.publisher("testPublisher")
+				.titleUrl("http://test.com")
+				.build();
+
+			given(bookRepository.searchBook(title, isbn, Pageable.unpaged()))
+				.willReturn(new PageImpl<>(List.of(getBookResponse)));
+
+			// when
+			PagedModel<GetBookResponse> result = bookQueryService.searchBook(searchBookRequest, Pageable.unpaged());
+
+			// then
+			assertNotNull(result);
+			assertEquals(getBookResponse.author(), result.getContent().getFirst().author());
+			assertEquals(getBookResponse.publisher(), result.getContent().getFirst().publisher());
+			assertEquals(getBookResponse.title(), result.getContent().getFirst().title());
+			assertEquals(getBookResponse.titleUrl(), result.getContent().getFirst().titleUrl());
+			assertEquals(getBookResponse.bookId(), result.getContent().getFirst().bookId());
+			assertEquals(1, result.getContent().size());
+		}
+	}
+
+}
