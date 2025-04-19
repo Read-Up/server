1f2bd5129f54ead6093e22affa784fd444da16f8
이주희 <twoweekhee@ijuhuiui-Macmini.local>
2025-04-19 15:39:27 +0900

    feature: add schema user, social account

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS `chapter`;
 DROP TABLE IF EXISTS `book`;
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `social_account`;
 
 CREATE TABLE `book`
 (
@@ -35,6 +37,39 @@ CREATE TABLE `chapter`
     PRIMARY KEY (`id`),
     KEY `fk_chapter_book_id` (`book_id`),
     CONSTRAINT `fk_chapter_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE user
+(
+    `id`         bigint       NOT NULL AUTO_INCREMENT,
+    `nickname`   varchar(255) NOT NULL,
+    `created_at` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_by` varchar(255)          DEFAULT NULL,
+    `updated_at` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated_by` varchar(255)          DEFAULT NULL,
+    `deleted_at` timestamp             DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE `social_account`
+(
+    `id`           bigint       NOT NULL AUTO_INCREMENT,
+    `user_id`      bigint                DEFAULT NULL,
+    `email`        varchar(255) NOT NULL,
+    `provider`     varchar(255) NOT NULL,
+    `provider_uid` varchar(255) NOT NULL,
+    `created_at`   timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_by`   varchar(255)          DEFAULT NULL,
+    `updated_at`   timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated_by`   varchar(255)          DEFAULT NULL,
+    `deleted_at`   timestamp             DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_social_account_user_id` (`user_id`),
+    CONSTRAINT `fk_social_account_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
