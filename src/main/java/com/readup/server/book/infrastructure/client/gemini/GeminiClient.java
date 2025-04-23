7abc1db0d27a8b251f4bb6ec723dcfab69f618a1
POKUDING <yoseb95@gmail.com>
2025-04-23 18:34:39 +0900

    feat: GeminiClient #28

---
+++ src/main/java/com/readup/server/book/infrastructure/client/gemini/GeminiClient.java
@@ -0,0 +1,55 @@
+package com.readup.server.book.infrastructure.client.gemini;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.readup.server.book.application.client.ParseChapterClient;
+import com.readup.server.book.application.client.vo.ChapterVO;
+import com.readup.server.book.infrastructure.client.gemini.dto.GeminiRequest;
+import com.readup.server.book.infrastructure.client.gemini.dto.GeminiResponse;
+import com.readup.server.book.infrastructure.client.gemini.feign.GeminiFeignClient;
+
+import io.jsonwebtoken.lang.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class GeminiClient implements ParseChapterClient {
+	private final GeminiClientProperties geminiClientProperties;
+	private final GeminiFeignClient geminiFeignClient;
+	private final ObjectMapper objectMapper;
+
+	public List<ChapterVO> parseRawChapter(String rawChapter) {
+
+		if (Objects.isEmpty(rawChapter)) {
+			return List.of();
+		}
+
+		GeminiRequest geminiRequest = GeminiRequest.builder()
+			.systemInstruction(geminiClientProperties.getParseChapterPromptTemplate())
+			.contents(rawChapter)
+			.build();
+
+		GeminiResponse geminiResponse = geminiFeignClient.getGeminiResponse(
+			geminiClientProperties.getApiKey(),
+			geminiRequest
+		);
+
+		try {
+			return objectMapper.readValue(
+				geminiResponse.getText().replace("```json", "").replace("```", "").trim(),
+				new TypeReference<>() {
+				}
+			);
+		} catch (JsonProcessingException e) {
+			log.error("Error parsing JSON response: {}", e.getMessage());
+			return List.of();
+		}
+	}
+}
