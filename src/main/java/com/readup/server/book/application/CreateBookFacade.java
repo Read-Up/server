e91ba767326c3aa228885a8ee913d642ef04b987
POKUDING <yoseb95@gmail.com>
2025-04-23 18:16:20 +0900

    refactor: BookInfoService -> CreateBookFacade #28

---
+++ src/main/java/com/readup/server/book/application/CreateBookFacade.java
@@ -0,0 +1,38 @@
+package com.readup.server.book.application;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.readup.server.book.application.client.BookInfoClientRegistry;
+import com.readup.server.book.application.client.ParseChapterClient;
+import com.readup.server.book.application.client.vo.BookInfoVO;
+import com.readup.server.book.application.client.vo.ChapterVO;
+import com.readup.server.book.application.dto.RetrieveBookResponse;
+import com.readup.server.book.domain.Book;
+import com.readup.server.book.domain.service.BookDomainService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CreateBookFacade {
+	private final BookDomainService bookDomainservice;
+	private final BookInfoClientRegistry bookInfoClientRegistry;
+	private final ParseChapterClient parseChapterClient;
+
+	public RetrieveBookResponse createBook(String isbn) {
+
+		bookDomainservice.doesNotExistBookByIsbn(isbn);
+
+		BookInfoVO bookInfoVO = bookInfoClientRegistry.getDefaultBookInfoClient().getBookInfo(isbn);
+		List<ChapterVO> chapterVO = parseChapterClient.parseRawChapter(bookInfoVO.chapter());
+
+		Book book = bookInfoVO.toEntity();
+		book.updateChapterList(chapterVO.stream().map(ChapterVO::toEntity).toList());
+
+		Book savedBook = bookDomainservice.save(book);
+
+		return RetrieveBookResponse.from(savedBook);
+	}
+}
