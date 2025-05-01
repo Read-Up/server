2df350cd4d8ddec33495428a8aabc63df6c225b0
POKUDING <yoseb95@gmail.com>
2025-05-01 12:24:30 +0900

    test: BookInfoClientFailureTest #55

--- src/test/java/com/readup/server/book/infrastructure/client/bookinfo/nationallibraryofkorea/BookInfoNationalLibraryOfKoreaClientTest.java
+++ src/test/java/com/readup/server/book/infrastructure/client/bookinfo/nationallibraryofkorea/BookInfoNationalLibraryOfKoreaClientTest.java
@@ -3,6 +3,7 @@ package com.readup.server.book.infrastructure.client.bookinfo.nationallibraryofk
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,8 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.readup.server.WireMockSupport;
 import com.readup.server.book.application.client.vo.BookInfoVO;
+import com.readup.server.common.exception.ErrorCode;
+import com.readup.server.common.exception.FeignException;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,21 +25,6 @@ class BookInfoNationalLibraryOfKoreaClientTest extends WireMockSupport {
 	NationalLibraryOfKoreaProperties nationalLibraryOfKoreaProperties;
 
 	@Test
-	@DisplayName("국립도서관 클라이언트에서 책 정보를 가져오는 테스트")
-	void nationalLibraryOfKoreaClientGetBookInfo() {
-		//given
-		String isbn = "9788966262281";
-
-		//when
-		BookInfoVO bookInfoVO = nationalLibraryOfKoreaClient.getBookInfo(isbn);
-
-		//then
-		assertEquals(isbn, bookInfoVO.isbn());
-		assertNotNull(bookInfoVO.bookTitle());
-		assertNotNull(bookInfoVO.author());
-	}
-
-	@Test
 	@DisplayName("국립도서관 서비스 이름을 가져오는 테스트")
 	void nationalLibraryOfKoreaClientGetServiceName() {
 		//given
@@ -47,5 +35,37 @@ class BookInfoNationalLibraryOfKoreaClientTest extends WireMockSupport {
 
 		//then
 		assertEquals(expectedServiceName, serviceName);
+	}
+
+	@Nested
+	@DisplayName("국립도서관 클라이언트에서 책 정보를 가져오는 테스트")
+	class GetBookInfoTest {
+
+		@Test
+		@DisplayName("국립도서관 클라이언트에서 책 정보를 가져오기 성공 테스트")
+		void nationalLibraryOfKoreaClientGetBookInfoSuccessTest() {
+			//given
+			String isbn = "9788966262281";
+
+			//when
+			BookInfoVO bookInfoVO = nationalLibraryOfKoreaClient.getBookInfo(isbn);
+
+			//then
+			assertEquals(isbn, bookInfoVO.isbn());
+			assertNotNull(bookInfoVO.bookTitle());
+			assertNotNull(bookInfoVO.author());
+		}
+
+		@Test
+		@DisplayName("국립도서관 클라이언트에서 책 정보를 가져오기 실패 테스트 - 존재 하지 않는 책")
+		void nationalLibraryOfKoreaClientGetBookInfo() {
+			//given
+			String isbn = "1234567890123";
+
+			//when-then
+			Throwable throwable = assertThrows(FeignException.class,
+				() -> nationalLibraryOfKoreaClient.getBookInfo(isbn));
+			assertEquals(ErrorCode.EXTERNAL_BOOK_INFO_NOT_FOUND.getMessage(), throwable.getMessage());
+		}
 	}
 }
