4560b766ab987fba4cf73ec5c9fa16840674cf49
Hut234 <doitdoitdoitdoitdo@gmail.com>
2025-05-14 18:43:23 +0900

    feature: Add UserQuizSet, UserQuiz Entity

---
+++ src/main/java/com/readup/server/user_quiz/domain/model/UserQuiz.java
@@ -0,0 +1,57 @@
+package com.readup.server.user_quiz.domain.model;
+
+import static jakarta.persistence.FetchType.*;
+import static lombok.AccessLevel.*;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.readup.server.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "user_quiz")
+@SQLRestriction("deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE user_quiz SET deleted_at = NOW() WHERE id = ?")
+@AllArgsConstructor(access = PRIVATE)
+@NoArgsConstructor(access = PROTECTED)
+public class UserQuiz extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(nullable = false)
+	private Long quizId;
+
+	@Column(nullable = false)
+	private int attemptCount;
+
+	@Column
+	private Boolean isCorrect;
+
+	@ManyToOne(fetch = LAZY)
+	private UserQuizSet userQuizSet;
+
+	public static UserQuiz create(Long quizId, UserQuizSet userQuizSet) {
+		return UserQuiz.builder()
+			.quizId(quizId)
+			.attemptCount(0)
+			.userQuizSet(userQuizSet)
+			.build();
+	}
+}
---
+++ src/main/java/com/readup/server/user_quiz/domain/model/UserQuizSet.java
@@ -0,0 +1,72 @@
+package com.readup.server.user_quiz.domain.model;
+
+import static jakarta.persistence.CascadeType.*;
+import static lombok.AccessLevel.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.readup.server.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "user_quiz_set")
+@SQLRestriction("deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE user_quiz_set SET deleted_at = NOW() WHERE id = ?")
+@AllArgsConstructor(access = PRIVATE)
+@NoArgsConstructor(access = PROTECTED)
+public class UserQuizSet extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(nullable = false)
+	private Long quizSetId;
+
+	@Column(nullable = false)
+	private Boolean isEvaluated;
+
+	@Column(nullable = false)
+	private int quizSequence;
+
+	@Column
+	private Double correctAnswerAverage;
+
+	@Column
+	private Integer likeScore;
+
+	@OneToMany(mappedBy = "userQuizSet", cascade = ALL, orphanRemoval = true)
+	private List<UserQuiz> userQuizList;
+
+	public static UserQuizSet create(Long quizSetId) {
+		return UserQuizSet.builder()
+			.quizSetId(quizSetId)
+			.isEvaluated(false)
+			.quizSequence(1)
+			.userQuizList(new ArrayList<>())
+			.build();
+	}
+
+	public void addUserQuiz(Long quizId) {
+		UserQuiz userQuiz = UserQuiz.create(quizId, this);
+		userQuizList.add(userQuiz);
+	}
+}
