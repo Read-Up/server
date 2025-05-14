4b1b4a753df6167b42ef989548cbf977bc194555
Hut234 <doitdoitdoitdoitdo@gmail.com>
2025-05-14 18:48:24 +0900

    feature: Add controller, service, repository for get user quiz set

---
+++ src/main/java/com/readup/server/user_quiz/application/UserQuizSetService.java
@@ -0,0 +1,50 @@
+package com.readup.server.user_quiz.application;
+
+import static com.readup.server.common.exception.ErrorCode.*;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.readup.server.common.exception.ServiceException;
+import com.readup.server.quiz.domain.model.Quiz;
+import com.readup.server.quiz.domain.model.QuizSet;
+import com.readup.server.quiz.domain.repository.QuizSetRepository;
+import com.readup.server.user.dto.AuthUser;
+import com.readup.server.user_quiz.application.dto.GetUserQuizSetResponse;
+import com.readup.server.user_quiz.domain.model.UserQuizSet;
+import com.readup.server.user_quiz.domain.repository.UserQuizSetRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserQuizSetService {
+
+	private final QuizSetRepository quizSetRepository;
+	private final UserQuizSetRepository userQuizSetRepository;
+
+	@Transactional
+	public GetUserQuizSetResponse getUserQuizSet(Long quizSetId, AuthUser user) {
+		QuizSet quizSet = getQuizSet(quizSetId);
+		UserQuizSet userQuizSet = getOrCreateUserQuizSet(quizSetId, user.id(), quizSet.getQuizList());
+		return GetUserQuizSetResponse.from(userQuizSet);
+	}
+
+	private UserQuizSet getOrCreateUserQuizSet(Long quizSetId, Long userId, List<Quiz> quizList) {
+		return userQuizSetRepository.findByQuizSetIdAndCreatedBy(quizSetId, userId)
+			.orElseGet(() -> createNewUserQuizSet(quizSetId, quizList));
+	}
+
+	private UserQuizSet createNewUserQuizSet(Long quizSetId, List<Quiz> quizList) {
+		UserQuizSet newUserQuizSet = UserQuizSet.create(quizSetId);
+		quizList.forEach(quiz -> newUserQuizSet.addUserQuiz(quiz.getId()));
+		return userQuizSetRepository.save(newUserQuizSet);
+	}
+
+	private QuizSet getQuizSet(Long quizSetId) {
+		return quizSetRepository.findById(quizSetId)
+			.orElseThrow(() -> new ServiceException(NOT_FOUND_QUIZ_SET));
+	}
+}
---
+++ src/main/java/com/readup/server/user_quiz/presentation/UserQuizSetController.java
@@ -0,0 +1,26 @@
+package com.readup.server.user_quiz.presentation;
+
+import static com.readup.server.common.dto.ApiResponse.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.readup.server.auth.annotaion.CurrentUser;
+import com.readup.server.common.dto.ApiResponse;
+import com.readup.server.user.dto.AuthUser;
+import com.readup.server.user_quiz.application.UserQuizSetService;
+import com.readup.server.user_quiz.application.dto.GetUserQuizSetResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class UserQuizSetController {
+
+	private final UserQuizSetService userQuizSetService;
+
+	@GetMapping("/private/user-quiz-set")
+	public ApiResponse<GetUserQuizSetResponse> getUserQuizSet(Long quizSetId, @CurrentUser AuthUser user) {
+		return successResponse(userQuizSetService.getUserQuizSet(quizSetId, user));
+	}
+}
