19b0e237be9f504af087c422ce26d5d0ba2718ef
Hut234 <doitdoitdoitdoitdo@gmail.com>
2025-07-19 13:36:14 +0900

    feature: Add quiz explanation controller, service and repository

--- src/main/java/com/readup/server/quiz/application/QuizSetService.java
+++ src/main/java/com/readup/server/quiz/application/QuizSetService.java
@@ -10,10 +10,12 @@ import com.readup.server.book.domain.repository.BookRepository;
 import com.readup.server.common.exception.ServiceException;
 import com.readup.server.quiz.application.dto.CreateQuizSetRequest;
 import com.readup.server.quiz.application.dto.CreateQuizSetResponse;
+import com.readup.server.quiz.application.dto.GetQuizExplanationResponse;
 import com.readup.server.quiz.application.dto.GetQuizSetPageResponse;
 import com.readup.server.quiz.application.dto.GetQuizSetResponse;
 import com.readup.server.quiz.application.dto.SliceResponse;
 import com.readup.server.quiz.domain.model.QuizSet;
+import com.readup.server.quiz.domain.repository.QuizQueryRepository;
 import com.readup.server.quiz.domain.repository.QuizSetRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,7 @@ public class QuizSetService {
 
 	private final BookRepository bookRepository;
 	private final QuizSetRepository quizSetRepository;
+	private final QuizQueryRepository quizQueryRepository;
 
 	@Transactional
 	public CreateQuizSetResponse createQuizSet(CreateQuizSetRequest request) {
@@ -55,6 +58,11 @@ public class QuizSetService {
 	public SliceResponse<GetQuizSetPageResponse> getParticipatingQuizSets(Long socialAccountId, Long bookId,
 		Long chapterId, Pageable pageable) {
 		return quizSetRepository.getParticipatingQuizSets(socialAccountId, bookId, chapterId, pageable);
+	}
+
+	@Transactional(readOnly = true)
+	public GetQuizExplanationResponse getQuizExplanation(Long quizId) {
+		return GetQuizExplanationResponse.from(quizQueryRepository.getQuizById(quizId));
 	}
 
 	private void validateChapter(Long bookId, Long chapterId) {
